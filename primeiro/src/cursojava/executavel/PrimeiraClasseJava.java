657d38f2f1d9a1057149a6a0aa42fc3b70e01f4f
Manoel Santiago <manoel.santiago1@gmail.com>
2026-04-03 21:01:25 -0500

    primeiro comit no git

---
+++ primeiro/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*main é um método auto executável*/
+	public static void main(String[] args) {
+		
+		//escreve texto no console
+		System.out.println("Meu primeiro programa em Java - do curso Java Web.");
+	}
+}
